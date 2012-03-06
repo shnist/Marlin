fedab8181dff5fb98ce1c9a6b14c47ba94ae9c96
Aaron Faber <shnist@btinternet.com>
2012-03-06 09:50:03 +0000

    added hidden input which tells us which chart is being asked for in the ajax request

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,72 +1,56 @@
 var chart = window.chart || {};
-var jsonData = [{
-	'timeStamp' : 'commit one',
-	'stylesheetsAtTop' : 25,
-	'avoidCSSImport' : 20
-},
-{
-	'timeStamp' : 'commit two',
-	'stylesheetsAtTop' : 40,
-	'avoidCSSImport' : 25
-},
-{
-	'timeStamp' : 'commit three',
-	'stylesheetsAtTop' : 25,
-	'avoidCSSImport' : 20
-},
-{
-	'timeStamp' : 'commit four',
-	'stylesheetsAtTop' : 50,
-	'avoidCSSImport' : 25
-}];
-var bestData = [{
-	'timeStamp' : 'commit one',
-	'avoidLongRunningScripts' : 100,
-	'deferParsingJs' : 65
-},
-{
-	'timeStamp' : 'commit two',
-	'avoidLongRunningScripts' : 90,
-	'deferParsingJs' : 70
-},
-{
-	'timeStamp' : 'commit three',
-	'avoidLongRunningScripts' : 85,
-	'deferParsingJs' : 80
-},
-{
-	'timeStamp' : 'commit four',
-	'avoidLongRunningScripts' : 100,
-	'deferParsingJs' : 80
-}];
-var jsData = [{
-	'timeStamp' : 'commit one',
-	'loadTime' : 100
-},
-{
-	'timeStamp' : 'commit two',
-	'loadTime' : 98
-},
-{
-	'timeStamp' : 'commit three',
-	'loadTime' : 80
-},
-{
-	'timeStamp' : 'commit four',
-	'loadTime' : 99
-}];
-
 
 chart = {
-	init : function (dimensions, wireframeNumber){
+	init : function (options){
 		// remove the tables
-		$('table', '.chart').addClass('hidden');
+		//$('table', '.chart').addClass('hidden');
 		
-		chart.drawChart(jsonData, 'HTTP Worst Performance', 'http-chart', dimensions, wireframeNumber);
-		chart.drawChart(jsData, 'JavaScript Performance', 'javascript-chart', dimensions, wireframeNumber);
-		chart.drawChart(bestData, 'HTTP Best Performance', 'best-http-chart', dimensions, wireframeNumber);
+		//console.log(options);
+		this.submit(options);
+		
+		//chart.drawChart(jsonData, options.name, 'http-chart', options.dimensions);
+		//chart.drawChart(jsData, 'JavaScript Performance', 'javascript-chart', dimensions);
+		//chart.drawChart(bestData, 'HTTP Best Performance', 'best-http-chart', dimensions);
 	},
-	drawChart : function (json, title, chartElement, dimensions, wireframeNumber){
+	/**
+	 * Submit
+	 * Method that captures form submissions and triggers AJAX request
+	 */
+	submit : function (option) {
+		$('.rule-options').submit(function (event) {
+			event.preventDefault();
+			var searchOptions = $(this).serialize();
+			console.log(searchOptions);
+			
+			
+			// send off ajax request
+			chart.retrieveData(searchOptions);
+		});
+	},
+	/**
+	 * Retrieve Data
+	 * Sends AJAX request to retrieve data from server
+	 */
+	retrieveData: function (searchOptions) {
+		$.ajax({
+			url : '',
+			data: searchOptions,
+			dataType: 'json',
+			type: 'post',
+			success : function (data) {
+				$('.overlay').remove();
+				$('#content.results').removeClass('ajax');
+				// results stored here
+				Boonbox.results = data;
+				Boonbox.filters.dom.resetResults();
+				Boonbox.filters.dom.addResults(data, 1);
+			},
+			error: function (object, stat, error) {
+				//console.log(stat + ': ' + error);
+			}
+		});
+	},
+	drawChart : function (json, title, chartElement, dimensions){
 		// new data table
 		var data = new google.visualization.DataTable(),
 			i = 0, j, k = 0, key, keys = [];
@@ -111,13 +95,8 @@ chart = {
 		}
 		
 		var chart = new google.visualization.LineChart(document.getElementById(chartElement));
-		if (wireframeNumber === 2){
-			chart.draw(data, {chartArea: {top: 10, left: 10}, width: dimensions[0], height: dimensions[1], title: title, vAxis:{maxValue: 100, minValue: 0}});
-		} else if(wireframeNumber === 3) {
-			chart.draw(data, {chartArea: {top: 50, left: 30}, legend: {position: 'top'},width: dimensions[0], height: dimensions[1], title:title, vAxis:{maxValue: 100, minValue: 0}});
-		} else {
-			chart.draw(data, {width: dimensions[0], height: dimensions[1], title: title, vAxis:{maxValue: 100, minValue: 0}});
-		}
+		chart.draw(data, {chartArea: {top: 50, left: 30}, legend: {position: 'top'},width: dimensions[0], height: dimensions[1], title:title, vAxis:{maxValue: 100, minValue: 0}});
+
 		
 	
 	}
