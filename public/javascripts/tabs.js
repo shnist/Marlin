b8c96c0ac57b7bb0e45c7befee94d3f16434f5a0
Aaron Faber <shnist@btinternet.com>
2012-03-07 17:40:11 +0000

    tabs successfully implemented

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -9,18 +9,20 @@ var tabs = window.tabs || {};
 tabs = {
 	/**
 	 * Creates the markup to inject the tabs
+	 * Changes href of tab navigation
 	 */
 	init : function () {
 		var tabs = '<div id="tab-two" class="tab-page"></div>'+
 		'<div id="tab-three" class="tab-page"></div>';
-		$('#content h2, .graph, .rule-options').wrapAll('<div id="tab-one" class="tab-page"></div>');
+		$('.rule-title, .graph, .rule-options').wrapAll('<div id="tab-one" class="tab-page"></div>');
 		$('#content').append(tabs);
 		$('.tab-navigation, .tab-page').wrapAll('<div id="tabs"></div>');
 		
-		//$('#tabs').tabs();
+		$('li:first-child a', '.tab-navigation').attr('href', '#tab-one');
+		$('li', '.tab-navigation').eq(1).children('a').attr('href', '#tab-two');
+		$('li:last-child a', '.tab-navigation').attr('href', '#tab-three');
 		
-		
-		
+		$('#tabs').tabs();
 	},
 	requestPage : function () {
 		
