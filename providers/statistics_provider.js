cdaf96e3f0575af85b384a73d615ff7c96af08b7
Aaron Faber <shnist@btinternet.com>
2012-03-10 16:43:29 +0000

    dates in date range are now dynamic

--- providers/statistics_provider.js
+++ providers/statistics_provider.js
@@ -52,8 +52,6 @@ StatisticsProvider.prototype.allProjectNames = function (callback) {
  * Find Statistics
  * Extracts all data from the chosen Statistics
  * Defaults:
- * 	DateTo: DATE = today
- * 	DateFrom: DATE = yesterday
  * 	ProjectName: STRING = name of project
  */
 
@@ -61,36 +59,37 @@ StatisticsProvider.prototype.findStatistics = function (parameters, callback) {
 	//86 400 000 = a day
 	var dateFrom = null,
 		dateTo = null,
-		projectName = '';
+		projectName = '',
+		query = null;
 		
 	if (typeof(parameters) !== 'object'){
 		// build default dates here,
 		// find is not filtered
-		dateFrom = (new Date().getTime() - 86400000);
-		dateTo = new Date().getTime();
 		projectName = parameters;
+		
+		query = Report.find({name: projectName}).limit(4);
 	} else {
 		// get dates from post data
 		dateFrom = new Date(parameters.dateFrom).getTime();
 		dateTo = new Date(parameters.dateTo).getTime();
 		projectName = parameters.name;
-	}
 		
-	Report.find({name: projectName})
-		.where('date').lte(dateTo)
-		.where('date').gt(dateFrom)
-	.run(function (err, docs){
-		if(!err){
+		query = Report.find({name: projectName}).where('date').lte(dateTo).where('date').gt(dateFrom)
+	}
+	
+	query.exec(function (error, docs) {
+		if(!error){
 			if (docs.length !== 0){				
 				callback(null, docs);	
 			} else {
 				callback('No results returned', null);
 			}
 		} else {
-			callback(err, null);
+			callback(error, null);
 		}
 	});
 }
+
 
 /**
  * Filter Rules
