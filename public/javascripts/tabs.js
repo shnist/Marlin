8656f14c0b75a99de71b7d9014cb5c663b42330d
Aaron Faber <shnist@btinternet.com>
2012-03-18 12:32:48 +0000

    fixed tab firing

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -11,22 +11,25 @@ tabs = {
 	 * Creates the markup to inject the tabs
 	 * Changes href of tab navigation
 	 */
-	init : function () {
-		var tabs = '<div id="tab-two" class="tab-page"></div>'+
-		'<div id="tab-three" class="tab-page"></div>';
-		$('.rule-title, .graph, .rule-options, .alerts').wrapAll('<div id="tab-one" class="tab-page"></div>');
-		$('#content').append(tabs);
-		$('.tab-navigation, .tab-page').wrapAll('<div id="tabs"></div>');
-		
-		$('li:first-child a', '.tab-navigation').attr('href', '#tab-one');
-		$('li', '.tab-navigation').eq(1).children('a').attr('href', '#tab-two');
-		$('li:last-child a', '.tab-navigation').attr('href', '#tab-three');
-		
-		$('body').append('<div id="island"></div>');
-		
-		$('#tabs').tabs();
-		
-		this.navEventHandlers();
+	init : function ($context) {
+		// test that tabs are needed
+		if($context.length > 0){
+			var tabs = '<div id="tab-two" class="tab-page"></div>'+
+			'<div id="tab-three" class="tab-page"></div>';
+			$('.rule-title, .graph, .rule-options, .alerts').wrapAll('<div id="tab-one" class="tab-page"></div>');
+			$('#content').append(tabs);
+			$('.tab-navigation, .tab-page').wrapAll('<div id="tabs"></div>');
+			
+			$('li:first-child a', '.tab-navigation').attr('href', '#tab-one');
+			$('li', '.tab-navigation').eq(1).children('a').attr('href', '#tab-two');
+			$('li:last-child a', '.tab-navigation').attr('href', '#tab-three');
+			
+			$('body').append('<div id="island"></div>');
+			
+			$('#tabs').tabs();
+			
+			this.navEventHandlers();
+		}
 	},
 	/**
 	 * Navigation Event Handlers
